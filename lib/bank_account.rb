563efe1cdba88bd89100e20386203068acf31bb9
anupama <anupamagadde@yahoo.com>
2019-12-25 21:32:48 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
+require "pry"
 class BankAccount
+
+    attr_accessor :balance, :status
+    #can't change its name
+    attr_reader :name
+#can initialize a Bank 
+#initializes with a name
+# always initializes with balance of 1000
+#always initializes with a status of 'open'
+    def initialize(name, balance = 1000)
+        @name = name
+        @balance = balance
+        #@balance = 1000
+        @status = "open"
+    end
+#can deposit money into its account
+    def deposit(amount)
+        @balance += amount
+    end
+# can display its balance
+    def display_balance
+      "Your balance is $#{@balance}."
+    end
+#is valid with an open status and a balance greater than 0
+    def valid?
+        if status == "open" && balance > 0 
+            true
+        else
+            false
+        end
+    end
+#can close its account
+    def close_account
+        @status = "closed"
+    end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,45 @@
+require "pry"
 class Transfer
   # your code here
+  attr_accessor :receiver, :sender, :amount, :status
+# can initialize a Transfer
+#initializes with a sender
+#initializes with a receiver
+#always initializes with a status of 'pending
+#initializes with a transfer amount
+  def initialize(sender, receiver,  amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"  
+  end
+#can check that both accounts are valid
+#calls on the sender and receiver's #valid? methods
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+#can execute a successful transaction between two accounts
+# each transfer can only happen once -- used if @status == "pending"
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+  def reverse_transfer
+     if @status == "complete"
+       @sender.balance += @amount
+       @receiver.balance -= @amount
+       @status = "reversed"
+     end
+
+  end
 end
