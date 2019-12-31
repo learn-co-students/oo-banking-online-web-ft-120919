ce4a7bf5ac74bc0401b8435c3339692f36573433
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-31 01:04:17 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,10 +18,23 @@ class Transfer
   end
   
   def execute_transaction
-    if self.valid? == true 
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
+    if @sender.balance > @amount && @status == "pending" && self.valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
       @status = "complete"
+    
+    else @sender.valid? == false
+      @sender.close_account
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance += amount
+      self.receiver.balance -= amount
+      self.status = "reversed"
     end
   end
   
