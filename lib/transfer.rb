c58a155a01cdfc5ea97ff5fc82c11da71c92ca92
github username <github email address>
2020-01-09 13:29:58 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, status)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = 50
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  # can execute a successful transaction between two accounts
+  # each transfer can only happen once
+  # rejects a transfer if the sender does not have enough funds 
+  # (does not have a valid account)
+
+  def execute_transaction
+    
+    sender.balance -= amount
+    receiver.balance += amount
+    @status = "complete"
+
+    if
+    sender.balance <= amount
+      "Transaction rejected. Please check your account balance."
+    end
+    # binding.pry
+  end
+  
+
+
 end
